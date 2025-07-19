90caa21d488fd352d3a1e9aaa437e972eb7f6cb5
user01 <asm777@ukr.net>
2025-07-19 20:59:16 +0300

    Command line parameters was parsed

--- main.cpp
+++ main.cpp
@@ -1,9 +1,60 @@
+#include "my-tools.h"
 #include <iostream>
+#include <string.h>
 
 using namespace std;
 
-int main()
+char fileName[500];
+unsigned int loAddr, hiAddr;
+
+int main(int nParams, char** params)
 {
-    cout << "Hello world!" << endl;
+    // Єдиний нульовий параметр - назва самої програми (зі шляхом)
+    if (nParams == 1){
+        cout << "***********" << endl;
+        cout << "* Довідка *" << endl;
+        cout << "***********" << endl;
+        cout << "Без параметрів - читати цю довідку" << endl;
+        cout << "Перший параметр - назва файла, який ми хочемо попазати/редагувати" << endl;
+        cout << "Другий параметр - адреса всередині файла, звідки почати (hex-число: наприклад: 0xA25FFBD8)" << endl;
+        cout << "                  Якщо не вказано, то - з початку файла" << endl;
+        cout << "Тратій параметр - адреса всередині файла, де закінчити  (hex-число: наприклад: 0xA25FFBD8)" << endl;
+        cout << "                  Якщо не вказано, то - до кінця файла" << endl;
+
+        return 1;
+    }
+
+    // Нульовий параметр - назва самої програми (зі шляхом)
+    // Пеший параметр - назва файла, який ми хочемо попазати/редагувати
+    if (nParams >= 2){
+        strcpy(fileName, params[1]);
+        if (!fileExists(fileName)){
+            cout << " File not exists: " << fileName << endl;
+            return 2;
+        }
+    }
+
+    if (nParams >= 3){
+        if (!isStrShowsHex(params[2])){
+            printf("Не вдалося зчитати/зрозуміти hex-значення для нижньої адреси: %s\n", params[2]);
+            return 3;
+        }
+        sscanf(params[2], "%x", &loAddr);
+    }
+
+    if (nParams >= 4){
+        if (!isStrShowsHex(params[3])){
+            printf("Не вдалося зчитати/зрозуміти hex-значення для нижньої адреси: %s\n", params[3]);
+            return 3;
+        }
+        sscanf(params[3], "%x", &hiAddr);
+    }
+
+//  Контрольна точка - перевірка як і які параметри ми проситали
+//    cout << fileName << endl;
+//    cout <<hex<< loAddr << endl;
+//    cout <<hex<< hiAddr << endl;
+
+
     return 0;
 }
